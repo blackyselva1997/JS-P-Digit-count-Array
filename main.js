976fbbae1ca3e0d2c216944e9ff5e9090ca37047
blackyselva <selvam17196@gmail.com>
2023-03-16 07:56:45 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,9 @@
+// 2. Design a method to count how many digits in the array (digits not number).
+
+let name1 = ["selvakumar","selva","ranjith"];
+let x = [];
+
+for (i=0;i<name1.length;i++) {
+    x.push(name1[i].length);
+}
+console.log(x);
